74201f6e260f3a34abb45f8f1a207d0689c1bc37
Arthur Efremenko <aefremenko24@gmail.com>
2024-11-18 11:41:08 -0500

    Initial Commit

---
+++ qhelper/qhelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  qhelperApp.swift
+//  qhelper
+//
+//  Created by Arthur Efremenko on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct qhelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
