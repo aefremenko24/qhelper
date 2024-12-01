4df190ed733cc60c0ec0d6b09dbe493b71280f54
Arthur Efremenko <aefremenko24@gmail.com>
2024-12-01 00:07:58 -0500

    Increase font size and add padding to DropView

--- qhelper/Views/DropView.swift
+++ qhelper/Views/DropView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  DropView.swift
 //  qhelper
 //
 //  Created by Arthur Efremenko on 11/18/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DropView: View {
     @ObservedObject var files: Files
     @State var isDisplayingAlert: Bool = false
     
@@ -16,15 +16,20 @@ struct ContentView: View {
         
         VStack {
             Image(systemName: "lightbulb")
-                .imageScale(.large)
+                .font(.system(size: 20))
                 .foregroundStyle(.tint)
-            Text("Drop the cue sheet here!")
+            Text("Drop your cue sheets here!")
+                .font(.system(size: 16, weight: .bold))
+            Text("\(files.files.count) sheets successfully added")
+                .padding()
+                .foregroundColor(.gray)
         }
+        .padding()
         .onDrop(of: [.fileURL], isTargeted: nil) { providers in
             if let provider = providers.first(where: { $0.canLoadObject(ofClass: URL.self) } ) {
                 let _ = provider.loadObject(ofClass: URL.self) { object, error in
                     if let url = object {
-                        var newFile = File(path: url.path(percentEncoded: false), name: url.lastPathComponent)
+                        let newFile = File(path: url.path(percentEncoded: false), name: url.lastPathComponent)
                         if url.pathExtension != "xlsx" {
                             isDisplayingAlert = true
                             return
@@ -48,4 +53,8 @@ struct ContentView: View {
             Alert(title: Text("Error Processing File"), message: Text("An error occurred while processing the file, make sure it is a valid and not corrupted cue sheet."), dismissButton: .cancel())
         }
     }
+}
+
+#Preview {
+    DropView(files: Files(), isDisplayingAlert: false)
 }
