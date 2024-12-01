35a65412505080fa09db9e726b5c01f96dac3fae
Arthur Efremenko <aefremenko24@gmail.com>
2024-12-01 00:07:05 -0500

    Add a settings view for specifying user preferences

--- qhelper/Parser/Validations.swift
+++ qhelper/Parser/Validations.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+/**
+ Validates that the host provided is not empty.
+ 
+ - Parameter host: Host to be validated.
+ - Returns: true if the host is valid, false otherwise.
+ */
+func validateHost(host: String) -> Bool {
+    return !host.isEmpty
+}
+
+/**
+ Validates that the provided port number is of length 1 through 5 characters, inclusive, and only contains numbers.
+ 
+ - Parameter port: Port to be validated.
+ - Returns: true if the port is valid, false otherwise.
+ */
+func validatePort(port: String) -> Bool {
+    if port.count < 1 || port.count > 5 {
+        return false
+    }
+    if !CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: port)) {
+        return false
+    }
+    return true
+}
+
+/**
+ Validates that the provided QLab passcode only contains numbers.
+ 
+ - Parameter passcode: Passcode to be validated.
+ - Returns: true if the passcode is valid, false otherwise.
+ */
+func validatePassCode(passcode: String) -> Bool {
+    return CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: passcode))
+}
+
+/**
+ Validates that the workspace name provided is not empty.
+ 
+ - Parameter workspace: Workspace name to be validated.
+ - Returns: true if the workspace name is valid, false otherwise.
+ */
+func validateWorkspace(workspace: String) -> Bool {
+    return !workspace.isEmpty
+}
--- qhelper/Views/SettingsView.swift
+++ qhelper/Views/SettingsView.swift
@@ -8,11 +8,101 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @ObservedObject var config: UserConfiguration
+    @State private var selectedCueType: CueType = .MIDI
+    @Environment(\.scenePhase) private var scenePhase
+    let saveAction: ()->Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("QLab Parameters")
+            .font(.system(size: 20, weight: .bold))
+            .padding()
+        
+        Form {
+            TextField(
+                text: $config.workspace,
+                prompt: Text("Required")
+            ) {
+                Text("Workspace Name")
+            }
+            .onSubmit {
+                if validateWorkspace(workspace: config.workspace) {
+                    saveAction()
+                } else {
+                    self.config.workspace = ""
+                }
+            }
+            .disableAutocorrection(true)
+            .padding()
+            
+            TextField(
+                text: $config.host,
+                prompt: Text("Required")
+            ) {
+                Text("Host")
+            }
+            .onSubmit {
+                if validateHost(host: config.host) {
+                    saveAction()
+                } else {
+                    self.config.host = DEFAULT_HOST
+                }
+            }
+            .disableAutocorrection(true)
+            .padding()
+            
+            TextField(
+                text: $config.port,
+                prompt: Text("Required")
+            ) {
+                Text("Port")
+            }
+            .onSubmit {
+                if validatePort(port: config.port) {
+                    saveAction()
+                } else {
+                    self.config.port = DEFAULT_LISTENING_PORT
+                }
+            }
+            .disableAutocorrection(true)
+            .padding()
+            
+            SecureField(
+                text: $config.passcode,
+                prompt: Text("Optional")
+            ) {
+                Text("Passcode")
+            }
+            .onSubmit {
+                if validatePassCode(passcode: config.passcode) {
+                    saveAction()
+                } else {
+                    self.config.passcode = ""
+                }
+            }
+            .disableAutocorrection(true)
+            .padding()
+            
+            Picker("Timed Cue Type", selection: $config.cue_type) {
+                ForEach(CueType.allCases, id: \.self) {
+                    Text($0.rawValue)
+                }
+            }
+            .onChange(of: config.cue_type) {
+                saveAction()
+            }
+            .padding()
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .inactive { saveAction() }
+        }
+        
+        Text("Your changes are saved automatically!")
+            .padding()
     }
 }
 
 #Preview {
-    SettingsView()
+    var config = UserConfiguration()
+    SettingsView(config: config, saveAction: {})
 }
