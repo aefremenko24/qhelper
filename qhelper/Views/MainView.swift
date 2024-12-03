ac4c1a96a64c39fb7e8612ba7c9654de785067ea
Arthur Efremenko <aefremenko24@gmail.com>
2024-12-02 19:08:18 -0500

    Implement TabView and support for macOS 13

---
+++ qhelper/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  qhelper
+//
+//  Created by Arthur Efremenko on 12/2/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
