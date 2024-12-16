72204e0df34779a6138bb541fe213ddf19f7e796
Arthur Efremenko <aefremenko24@gmail.com>
2024-12-15 22:59:14 -0500

    Only display two options for timed cue type: MIDI and Network

--- qhelper/Views/SettingsView.swift
+++ qhelper/Views/SettingsView.swift
@@ -38,14 +38,16 @@ struct SettingsView: View {
                 .padding()
                 
                 Picker("Timed Cue Type", selection: $config.cue_type) {
-                    ForEach(CueType.allCases, id: \.self) {
-                        Text($0.rawValue)
-                    }
+                    Text("MIDI")
+                        .tag(CueType.MIDI)
+                    Text("Network")
+                        .tag(CueType.NETWORK)
                 }
                 .onChange(of: config.cue_type) { _ in
                     saveAction()
                 }
                 .padding()
+                .pickerStyle(.segmented)
                 
                 Toggle(isOn: $config.include_blackout_cue) {
                     Text("Blackout cues before cue groups")
@@ -63,53 +65,55 @@ struct SettingsView: View {
                 DisclosureGroup(
                     isExpanded: $showAdvancedSettings,
                     content: {
-                        TextField(
-                            text: $config.host,
-                            prompt: Text("Required")
-                        ) {
-                            Text("Host")
-                        }
-                        .onSubmit {
-                            if validateHost(host: config.host) {
-                                saveAction()
-                            } else {
-                                self.config.host = DEFAULT_HOST
+                        Form {
+                            TextField(
+                                text: $config.host,
+                                prompt: Text("Required")
+                            ) {
+                                Text("Host")
                             }
-                        }
-                        .disableAutocorrection(true)
-                        .padding()
-                        
-                        TextField(
-                            text: $config.send_port,
-                            prompt: Text("Required")
-                        ) {
-                            Text("Port")
-                        }
-                        .onSubmit {
-                            if validatePort(port: config.send_port) {
-                                saveAction()
-                            } else {
-                                self.config.send_port = DEFAULT_LISTENING_PORT
+                            .onSubmit {
+                                if validateHost(host: config.host) {
+                                    saveAction()
+                                } else {
+                                    self.config.host = DEFAULT_HOST
+                                }
                             }
-                        }
-                        .disableAutocorrection(true)
-                        .padding()
-                        
-                        SecureField(
-                            text: $config.passcode,
-                            prompt: Text("Optional")
-                        ) {
-                            Text("Passcode")
-                        }
-                        .onSubmit {
-                            if validatePassCode(passcode: config.passcode) {
-                                saveAction()
-                            } else {
-                                self.config.passcode = ""
+                            .disableAutocorrection(true)
+                            .padding()
+                            
+                            TextField(
+                                text: $config.send_port,
+                                prompt: Text("Required")
+                            ) {
+                                Text("Port")
                             }
+                            .onSubmit {
+                                if validatePort(port: config.send_port) {
+                                    saveAction()
+                                } else {
+                                    self.config.send_port = DEFAULT_LISTENING_PORT
+                                }
+                            }
+                            .disableAutocorrection(true)
+                            .padding()
+                            
+                            SecureField(
+                                text: $config.passcode,
+                                prompt: Text("Optional")
+                            ) {
+                                Text("Passcode")
+                            }
+                            .onSubmit {
+                                if validatePassCode(passcode: config.passcode) {
+                                    saveAction()
+                                } else {
+                                    self.config.passcode = ""
+                                }
+                            }
+                            .disableAutocorrection(true)
+                            .padding()
                         }
-                        .disableAutocorrection(true)
-                        .padding()
                     },
                     label: {
                         Text("Advanced Settings")
@@ -127,6 +131,8 @@ struct SettingsView: View {
 }
 
 #Preview {
-    var config = UserConfiguration()
-    SettingsView(config: config, saveAction: {})
+    SettingsView(config: UserConfiguration()) {
+        Task {}
+    }
+    .frame(width: WINDOW_WIDTH - PADDING, height: WINDOW_HEIGHT - PADDING)
 }
