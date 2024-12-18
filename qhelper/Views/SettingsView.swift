1f6722f9b769b6b91091600cf7f01f9f29449e54
Arthur Efremenko <aefremenko24@gmail.com>
2024-12-18 16:35:43 -0500

    Add popovers for extra explanation of fields in settings.

--- qhelper/Views/SettingsView.swift
+++ qhelper/Views/SettingsView.swift
@@ -14,6 +14,11 @@ struct SettingsView: View {
     @Environment(\.scenePhase) private var scenePhase
     let saveAction: ()->Void
     
+    @State var presentWorkspaceNamePopover = false
+    @State var presentCueTypePopover = false
+    @State var presentBlackoutCuesPopover = false
+    @State var presentAdvancedSettingsPopover = false
+    
     var body: some View {
         VStack {
             Text("QLab Parameters")
@@ -21,45 +26,109 @@ struct SettingsView: View {
                 .padding()
             
             Form {
-                TextField(
-                    text: $config.workspace,
-                    prompt: Text("Required")
-                ) {
-                    Text("Workspace Name")
-                }
-                .onSubmit {
-                    if validateWorkspace(workspace: config.workspace) {
-                        saveAction()
-                    } else {
-                        self.config.workspace = ""
+                HStack (spacing: 0) {
+                    TextField(
+                        text: $config.workspace,
+                        prompt: Text("Required")
+                    ) {
+                        Text("Workspace Name")
                     }
+                    .onSubmit {
+                        if validateWorkspace(workspace: config.workspace) {
+                            saveAction()
+                        } else {
+                            self.config.workspace = ""
+                        }
+                    }
+                    .disableAutocorrection(true)
+                    .padding()
+                    
+                    Button {
+                        presentWorkspaceNamePopover = true
+                    }
+                    label: {
+                        Image(systemName: "questionmark.circle")
+                            .popover(
+                                isPresented: $presentWorkspaceNamePopover, arrowEdge: .leading
+                            ) {
+                                Text("""
+                                     Workspace name may be either the 
+                                     display name of the workspace, such as 
+                                     'NUDANCO_Showcase', or the unique ID of 
+                                     the workspace, which can be found in the 
+                                     Info tab of the Workspace Status Window.
+                                    """)
+                                .padding()
+                            }
+                    }
+                    .buttonStyle(.borderless)
                 }
-                .disableAutocorrection(true)
-                .padding()
                 
-                Picker("Timed Cue Type", selection: $config.cue_type) {
-                    Text("MIDI")
-                        .tag(CueType.MIDI)
-                    Text("Network")
-                        .tag(CueType.NETWORK)
+                HStack (spacing: 0) {
+                    Picker("Timed Cue Type", selection: $config.cue_type) {
+                        Text("MIDI")
+                            .tag(CueType.MIDI)
+                        Text("Network")
+                            .tag(CueType.NETWORK)
+                    }
+                    .onChange(of: config.cue_type) { _ in
+                        saveAction()
+                    }
+                    .padding()
+                    .pickerStyle(.segmented)
+                    
+                    Button {
+                        presentCueTypePopover = true
+                    }
+                    label: {
+                        Image(systemName: "questionmark.circle")
+                            .popover(
+                                isPresented: $presentCueTypePopover, arrowEdge: .leading
+                            ) {
+                                Text("""
+                                     Use MIDI in Blackman Auditorium
+                                     and Network in Fenway Center.
+                                    """)
+                                    .padding()
+                            }
+                    }
+                    .buttonStyle(.borderless)
                 }
-                .onChange(of: config.cue_type) { _ in
-                    saveAction()
-                }
-                .padding()
-                .pickerStyle(.segmented)
                 
-                Toggle(isOn: $config.include_blackout_cue) {
-                    Text("Blackout cues before cue groups")
-                    if config.include_blackout_cue {
-                        Text("Will include a blackout cue before each cue group")
-                    } else {
-                        Text("Will include a blackout cue inside each cue group")
+                HStack {
+                    Toggle(isOn: $config.include_blackout_cue) {
+                        Text("Blackout cues before cue groups")
+                        if config.include_blackout_cue {
+                            Text("Will include a blackout cue before each cue group")
+                        } else {
+                            Text("Will include a blackout cue inside each cue group")
+                        }
+                        
+                    }
+                    .onChange(of: config.include_blackout_cue) { _ in
+                        saveAction()
                     }
                     
-                }
-                .onChange(of: config.include_blackout_cue) { _ in
-                    saveAction()
+                    Button {
+                        presentBlackoutCuesPopover = true
+                    }
+                    label: {
+                        Image(systemName: "questionmark.circle")
+                            .popover(
+                                isPresented: $presentBlackoutCuesPopover, arrowEdge: .top
+                            ) {
+                                Text("""
+                                     Ask the ligting person working the shift!
+                                     If checked, the blackout cue will be added
+                                     before each cue group and will not be timed.
+                                     If not checked, the blackout cue will be 
+                                     included in the beginning of each group and 
+                                     will have a default time of 0.01s.
+                                    """)
+                                    .padding()
+                            }
+                    }
+                    .buttonStyle(.borderless)
                 }
                 
                 DisclosureGroup(
@@ -116,7 +185,28 @@ struct SettingsView: View {
                         }
                     },
                     label: {
-                        Text("Advanced Settings")
+                        HStack {
+                            Text("Advanced Settings")
+                            
+                            Button {
+                                presentAdvancedSettingsPopover = true
+                            }
+                            label: {
+                                Image(systemName: "questionmark.circle")
+                                    .popover(
+                                        isPresented: $presentAdvancedSettingsPopover, arrowEdge: .trailing
+                                    ) {
+                                        Text("""
+                                            If cues don't get added to QLab,
+                                            make sure these settings match 
+                                            the parameters set in the Network
+                                            tab in the QLab Settings.
+                                            """)
+                                            .padding()
+                                    }
+                            }
+                            .buttonStyle(.borderless)
+                        }
                     }
                 )
             }
